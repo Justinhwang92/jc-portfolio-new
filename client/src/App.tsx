1e2099b75ff0163c66eaebc66153487e71448790
justinhwang92 <justinhwang92@gmail.com>
2022-10-20 17:02:30 -0400

    Add project field

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ export default function App() {
       <Navbar />
       <AppWrap component={Header} idName="home" />
       <AppWrap component={About} idName="about" />
+      <AppWrap component={Work} idName="work" />
       {/* <Work />
       <Skills />
       <Testimonial />
