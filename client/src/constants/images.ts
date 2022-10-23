8082fdecceeb86564021df156eb56de01346ef7c
justinhwang92 <justinhwang92@gmail.com>
2022-10-23 17:34:46 -0400

    Optimize asset import

---
+++ client/src/constants/images.ts
@@ -0,0 +1,20 @@
+import email from '../assets/email.png';
+import mobile from '../assets/mobile.png';
+import mongodb from '../assets/mongodb.svg';
+import graphql from '../assets/graphql.png';
+import react from '../assets/react.svg';
+
+import profile from '../assets/profile.png';
+import circle from '../assets/circle.svg';
+import logo from '../assets/logo.png';
+
+export default {
+  email,
+  mobile,
+  mongodb,
+  graphql,
+  react,
+  profile,
+  circle,
+  logo,
+};
